2b2c54b2ad17f205956eddb5bab00e1475ff7a86
Tóth Bendegúz <toth.bendeguz@students.jedlik.eu>
2023-10-16 17:19:18 +0200

    v0.16 Hozzáadott defek + javítások egy két helyen

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import os
 #from helyszínek.csarnok import csarnok
 
 zseblampa = False
-kabát = False
+kabat = False
 labirintusKod = False
+hp = 100
+józanság = 100
 
 
 
@@ -25,18 +27,19 @@ def slowPrint(string, speed=0.05):
         sys.stdout.flush()
         time.sleep(speed)
 
-#slowPrint('1925-ben, egy hideg téli este, a falu szélén magasodott az elhagyatott kastély. A vidék fehér hóval volt bevonva, és a kastély sötét, hátborzongató alakja feltűnt a horizonton. Az épület régóta lakatlan volt, és hosszú ideje tartották hírhedtnek a helyiek. A rémtörténetek egytől-egyig sötét eseményekről szóltak, amelyek itt zajlottak, és az emberek úgy érezték, hogy a kastély titokzatos erőket rejteget. Most, a hóesésben, az emberek újra hallották a kastély suttogásait, és az idő eljött, hogy felderítsék a múlt sötét rejtélyeit. Az elhagyatott kastély magasodott a téli éjszaka közepén, készen arra, hogy felfedezzék a titkait, mielőtt a borzalmak újra elszabadulnak, és bekebelezik az éjszakát. Az elhagyatott kastély falai tele vannak titkokkal, és most a te feladatod felfedezni ezt a sötét és rejtélyes helyet.', 0.03)
+#slowPrint('1925-ben, egy hideg téli este, a falu szélén magasodott az elhagyatott kastély. A vidék fehér hóval volt bevonva, és a kastély sötét, hátborzongató alakja feltűnt a horizonton. Az épület régóta lakatlan volt, és hosszú ideje tartották hírhedtnek a helyiek. A rémtörténetek egytől-egyig sötét eseményekről szóltak, amelyek itt zajlottak, és az emberek úgy érezték, hogy a kastély titokzatos erőket rejteget. Most, a hóesésben, az emberek újra hallották a kastély suttogásait, és az idő eljött, hogy felderítsék a múlt sötét rejtélyeit. Az elhagyatott kastély magasodott a téli éjszaka közepén, készen arra, hogy felfedezzék a titkait, mielőtt a borzalmak újra elszabadulnak, és bekebelezik az éjszakát. Az elhagyatott kastély falai tele vannak titkokkal, és most a te feladatod felfedezni ezt a sötét és rejtélyes helyet. Amint belépsz a főbejáraton az ajtó mögötted örökre becsukódik.', 0.03)
 
 input("\nHa készen álasz nyomd meg az entert!")
 os.system('cls')
 
 def foBejarat():
+    os.system('cls')
     print('A főbejáratban vagy')
     print('1 >> Jobbra látsz egy pincét')
     print('2 >> előtted van egy hosszú folyosó')
     print('3 >> Balra Egy nagy csarnokba nyíl egy ajtó')
 
-    v = input()
+    v = input('Merre haladsz tovább?: >> ')
     match v:
         case '1':
             return 'pince'
@@ -46,22 +49,73 @@ def foBejarat():
             return 'csarnok'
 
 def pince():
+    os.system('cls')
     print('A pince bejáratábn vagy')
-    print('1 >> Jobbra látsz egy pincét')
-    print('2 >> előtted van egy hosszú folyosó')
-    print('3 >> Balra Egy nagy csarnokba nyíl egy ajtó')
+    print('1 >> Egy raktár ajtaja nyílik meg előtted ')
+    print('2 >> Egyenesen nézve látsz egy kriptát ami elég félelmetes és sötét')
+    print('3 >> A harmadik hely egy titkos labort rejt')
     print('4 >> Vissza a főbejárathoz')
 
-    v = input()
+    v = input('Merre haladsz tovább?: >> ')
     match v:
         case '1':
-            return '#'
+            os.system('cls')
+            print('A raktárban megtalálod az eldugott kabátot')
+            kabat = True
+            input("\nHa készen álasz nyomd meg az entert!")
+            return 'pince'
         case '2':
-            return '#'
+            return 'pince'
         case '3':
-            return '#'
+            return 'pince'
         case '4':
             return 'fobejarat'
+
+def folyoso():
+    os.system('cls')
+    print('A hosszú folyosón sétálsz')
+    print('1 >> Jobb kezednél meglátsz egy ajtót ami kintre vezet')
+    print('2 >> Tovább haladva egy konyhát látsz')
+    print('3 >> A konyha mellett egyből találsz egy étkezőt')
+    print('4 >> Vissza szaladsz a főbejárathoz')
+
+    v = input('Merre haladsz tovább?: >> ')
+    match v:
+        case '1':
+            if kabat == True:
+                os.system('cls')
+                print('Mivel volt kabátot sikeresen kitudtál menni az udvarra!')
+                input("\nHa készen álasz nyomd meg az entert!")
+                return 'udvar'
+            if kabat == False:
+                os.system('cls')
+                print('Kimentél de egyből vissza jöttél hiszen kabát nélkül fáztál. Keresgélj tovább!')
+                input("\nHa készen álasz nyomd meg az entert!")
+                return 'folyoso'
+        case '2':
+            return 'folyoso'
+        case '3':
+            return 'csarnok'
+        case '4':
+            return 'fobejarat'
+
+"""
+def csarnok():
+    os.system('cls')
+    print('A csarnokban vagy')
+    print('1 >> A csarnok elje')
+    print('2 >> A csarnok közepe')
+    print('3 >> A csarnok vége')
+
+    v = input('Merre haladsz tovább?: >> ')
+    match v:
+        case '1':
+            return 'cseleje'
+        case '2':
+            return ''
+        case '3':
+            return 'csarnok'
+"""
         
 
 
@@ -71,7 +125,9 @@ while hely != 'asd':
     match hely:
         case 'fobejarat':
             hely = foBejarat()
-        case 'asd':
-            hely = ()
+        case 'csarnok':
+            pass
         case 'pince':
             hely = pince()
+        case 'folyoso':
+            hely = folyoso()
